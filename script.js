682acea4902668ebd274e3e70c3f3873b9a1f7a1
thewton <thewnehring@gmail.com>
2020-10-28 19:55:47 -0400

    added prompts on button click, and if statements

--- script.js
+++ script.js
@@ -12,3 +12,35 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  
+  var passlength = prompt("How many characters between 8 and 128 do you want your password to be?"); 
+  var capital = confirm("Do you want capital letters?")
+  var lower = confirm("Do you want lower-case letters?");
+  var numbs = confirm("Do you want to include numbers?");
+  var special = confirm("Do you want to include special characters? (ex. #, !. $, %... etc.)");
+  
+  var passCharacters = ""
+
+  if (lower) {
+    passCharacters += "abcdefghijklmnopqrstuvwxyz";
+  }
+  if (capital) { 
+    passCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (numbs) {
+    passCharacters += "0123456789"; 
+  }
+  if (special) { 
+    passCharacters += "$%^&*()@!#-_=+/[]{|~`'";
+  }
+
+ 
+
+
+
+}
