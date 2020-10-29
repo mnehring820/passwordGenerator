d94807ffc091415dca3e20d3d564fb3603043dc1
thewton <thewnehring@gmail.com>
2020-10-28 20:28:20 -0400

    added if statements within loop for first question to prompt user to select number from correct range

--- script.js
+++ script.js
@@ -18,7 +18,37 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   
-  var passlength = prompt("How many characters between 8 and 128 do you want your password to be?"); 
+  do{
+    var passLength = prompt("How long should the password be? (8-128 characters)");
+
+    
+    if(passLength === null){
+      return("Cancelled");
+    }
+
+    if(passLength > 128){
+      alert("Must be a number 128 or less");
+    }
+
+    if(passLength < 8){
+      alert("Must be a number 8 or greater");
+    }
+  
+    if(isNaN(passLength) === true){
+      alert("Please select a number");
+    }
+   
+    if(Math.floor(passLength) !== Number(passLength)){
+      alert("Please choose an integer between 8 and 128");
+    }
+    
+    
+  } while (passLength < 8 || passLength > 128 || isNaN(passLength) === true || Math.floor(passLength) !== Number(passLength));
+
+
+
+
+
   var capital = confirm("Do you want capital letters?")
   var lower = confirm("Do you want lower-case letters?");
   var numbs = confirm("Do you want to include numbers?");
